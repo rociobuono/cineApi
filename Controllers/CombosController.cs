5bb640c0317eb4b4d97cb8f5d6ea67b60e8cd8b4
Hilet Aulas <hilet@HILET.LOCAL>
2024-05-09 09:06:25 -0300

    controller y model de combos

---
+++ Controllers/CombosController.cs
@@ -0,0 +1,103 @@
+using ATDapi.Responses;
+using ATDapi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace ATDapi.Controllers;
+
+[ApiController]
+public class CombosController : ControllerBase
+{
+    public static List<CombosModel> DataLista = new List<CombosModel>();
+
+    [HttpGet]
+    [Route("CombosController/Get")]
+    public BaseResponse Get(){
+        
+        return new DataResponse<List<CombosModel>>(true,(int)HttpStatusCode.OK,"Lista de combos",DataLista);
+    }
+
+    [HttpPost]
+    [Route("CombosController/Post")]
+
+    public BaseResponse Post([FromBody]CombosModel dataInput){
+        
+        dataInput.id = Guid.NewGuid();
+        DataLista.Add(dataInput);
+        return new BaseResponse (true,(int)HttpStatusCode.Created, "Combo creado"); 
+    }
+
+    [HttpDelete]
+    [Route("CombosController/Delete")]
+    public BaseResponse Delete([FromQuery]Guid id){
+        CombosModel? combo = DataLista.FirstOrDefault(x => x.id == id);
+
+        if (combo == null)
+        {
+            return new BaseResponse(false,(int)HttpStatusCode.NotFound,"No se encontro el combo");
+        }
+        else{
+            DataLista.Remove(combo);
+            return new BaseResponse(true, (int)HttpStatusCode.OK, "Combo eliminada correctamente");
+        }
+    }
+
+    [HttpPatch]
+    [Route("CombosController/Patch")]
+
+    public BaseResponse Patch([FromBody] CombosModel dataInput){
+
+        if (dataInput.id == null)
+        {
+            return new BaseResponse(false, (int)HttpStatusCode.BadRequest, "No se encontro el id");
+        }
+
+        CombosModel? combos = DataLista.FirstOrDefault(x => x.id == dataInput.id);
+        if (combos == null)
+        {
+            return new BaseResponse(false, (int)HttpStatusCode.NotFound, "No se encontro el combo");
+        }
+        else
+        {
+            DataLista.Remove(combos);
+            DataLista.Add(dataInput);
+            return new BaseResponse(true, (int)HttpStatusCode.OK, "Combo actualizada correctamente");
+        }
+    }
+
+    [HttpPut]
+    [Route("CombosController/Put")]
+
+    public BaseResponse Put([FromBody] CombosModel dataInput){
+        if(dataInput.id == null)
+        {
+            return new BaseResponse(false, (int)HttpStatusCode.BadRequest, "No se encontro el id");
+        }
+        CombosModel? combo = DataLista.FirstOrDefault(x => x.id == dataInput.id);
+        if(combo == null)
+        {
+            return new BaseResponse(false, (int)HttpStatusCode.NotFound, "El combo no fue encontrado");
+        }
+        else
+        {
+            DataLista.Remove(combo);
+            if(dataInput.titulo != null)
+            {
+                combo.titulo = dataInput.titulo;
+            }
+            if(dataInput.imagen != null)
+            {
+                combo.imagen = dataInput.imagen;
+            }
+            if(dataInput.descripcion != null)
+            {
+                combo.descripcion = dataInput.descripcion;
+            }
+            DataLista.Add(combo);
+            return new BaseResponse(true, (int)HttpStatusCode.OK, "Combo modificado");
+        }
+    }
+
+
+
+}
---
+++ Models/CombosModel.cs
@@ -0,0 +1,11 @@
+namespace ATDapi.Models;
+
+public class CombosModel{
+
+    public Guid? id {get; set;}
+    public string titulo {get; set;}
+    public string imagen { get; set; }
+    public string descripcion { get; set; }
+
+
+}
